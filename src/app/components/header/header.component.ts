5337e05e62b095e3547c474c9a50a28555d809ba
naopeke <naopeke@protonmail.com>
2025-03-26 09:39:08 +0100

    added components

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatButtonModule, MatMenuModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })

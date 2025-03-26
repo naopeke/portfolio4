6e75537fbbc62e1b3732d47a36938aa445c708cd
naopeke <naopeke@protonmail.com>
2025-03-26 13:02:09 +0100

    hero

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })

bec1216dae73cfb945ed7a8f34123b1cabb41e27
naopeke <naopeke@protonmail.com>
2025-03-27 15:16:07 +0100

    env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
     emailjs: {
-        serviceId: process.env['YOUR_SERVICE_ID'],
-        templateId: process.env['YOUR_TEMPLATE_ID'],
-        publicKey: process.env['YOUR_PUBLIC_KEY'],
+        serviceId: process.env['NG_APP_YOUR_SERVICE_ID'],
+        templateId: process.env['NG_APP_YOUR_TEMPLATE_ID'],
+        publicKey: process.env['NG_APP_YOUR_PUBLIC_KEY'],
     }
 };
